bfb60cbdf3d61a0d90726d1945f451a419f0913b
Omar Stefan Evans <omar@evansbros.info>
2014-12-13 13:50:04 -0500

    Initial Commit

---
+++ rc_malloc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  rc_malloc
+//
+//  Created by Omar Evans on 12/13/14.
+//  Copyright (c) 2014 Omar Evans. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
